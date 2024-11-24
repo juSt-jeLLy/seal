0f8d3778e43866d8489cc20bce4d0f62f8ab8a5f
juSt-jeLLy <yagneshpatel931@gmail.com>
2024-11-24 19:50:01 +0530

    debug

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,12 +1,8 @@
 "use client";
 
 import type { NextPage } from "next";
-import { useAccount } from "wagmi";
-import { Address } from "~~/components/scaffold-eth";
 
 const Home: NextPage = () => {
-  const { address: connectedAddress } = useAccount();
-
   return (
     <>
       <div className="flex items-center flex-col flex-grow pt-10">
